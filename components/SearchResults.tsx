635c81f1607ed952510f19ad39a0660410491473
Jack Allard <jackallard17@gmail.com>
2025-06-23 22:08:50 -0700

    refactor: extract search result items into TaxaCard component

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,7 +1,7 @@
 import styles from '@components/SearchResults.module.scss';
-import * as React from 'react';
+import TaxaCard from './TaxaCard';
 
-interface INaturalistResult {
+export interface INaturalistResult {
   id: number;
   rank: string;
   name: string;
@@ -30,26 +30,9 @@ export default function SearchResults(props: SearchResultsProps) {
         <div className={styles.searchResultsContainer}>
           <div key={rowIndex} className={styles.row}>
             {rowItems.map((result, columnIndex) => (
-              <div key={result.id} className={styles.column}>
-                {result.default_photo?.medium_url && <img src={result.default_photo.medium_url} alt={result.preferred_common_name || result.name} className={styles.image} />}
-                <div className={styles.content}>
-                  {result.preferred_common_name || result.name}
-                  <div className={styles.latin}>{!result.preferred_common_name || result.name}</div>
-                  <div className={styles.rank}>{result.rank}</div>
-                </div>
-                <div className={styles.title}>
-                  <div className={styles.links}>
-                    {result.wikipedia_url && (
-                      <a href={result.wikipedia_url} target="_blank" rel="noopener noreferrer" className={styles.link}>
-                        Wikipedia
-                      </a>
-                    )}
-                    <a href={`https://www.inaturalist.org/taxa/${result.id}`} target="_blank" rel="noopener noreferrer" className={styles.link}>
-                      iNaturalist
-                    </a>
-                  </div>
-                </div>
-              </div>
+              <>
+                <TaxaCard result={result}></TaxaCard>
+              </>
             ))}
           </div>
         </div>
