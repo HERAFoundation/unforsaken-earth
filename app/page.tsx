534469b116aca1542a89a023ad0371cfa629fbe0
Jack Allard <jackallard17@gmail.com>
2025-07-07 22:17:59 -0700

    feat: add centered standalone taxacard

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,17 @@ import * as Constants from '@common/constants';
 import * as Utilities from '@common/utilities';
 
 import Bluesky from '@system/svg/social/Bluesky';
-import DefaultLayout from '@components/DefaultLayout';
+import PageComponent from '@components/Page';
 import ListItem from '@components/ListItem';
 import Package from '@root/package.json';
 import Script from 'next/script';
+import TaxaCard, { TaxaCardProps } from '@root/components/TaxaCard';
+import searchStyles from '@components/SearchResults.module.scss';
 
 export async function generateMetadata({ params, searchParams }) {
   const title = Package.name;
   const description = Package.description;
   const url = 'https://unforsaken.earth';
-  const handle = '@jackallard.xyz';
 
   return {
     metadataBase: new URL('https://unforsaken.earth'),
@@ -26,28 +27,65 @@ export async function generateMetadata({ params, searchParams }) {
       description,
       url,
       type: 'website',
-      images: [
-        {
-          url: 'https://intdev-global.s3.us-west-2.amazonaws.com/template-twitter-summary-large.png',
-          width: 1200,
-          height: 628,
-        },
-      ],
+      images: [],
     },
     icons: {
       icon: '/favicon-32x32.png',
       shortcut: '/favicon-16x16.png',
-      apple: [{ url: '/apple-touch-icon.png' }, { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }],
-      other: [
-        {
-          rel: 'apple-touch-icon-precomposed',
-          url: '/apple-touch-icon-precomposed.png',
-        },
-      ],
+      other: [{}],
     },
   };
 }
 
 export default async function Page(props) {
-  return <DefaultLayout previewPixelSRC="https://intdev-global.s3.us-west-2.amazonaws.com/template-app-icon.png"></DefaultLayout>;
+  const title = Package.name;
+  const description = Package.description;
+  const url = 'https://unforsaken.earth';
+
+  // this is just for demonstration purposes
+  const ganodermaOregonense: TaxaCardProps = {
+    id: 118062,
+    rank: 'species',
+    name: 'Ganoderma oregonense',
+    preferred_common_name: 'West Coast Reishi',
+    default_photo: {
+      medium_url: 'https://inaturalist-open-data.s3.amazonaws.com/photos/249116347/medium.jpg',
+      attribution: '(c) scramblyn, some rights reserved (CC BY-NC)',
+    },
+    wikipedia_url: 'https://en.wikipedia.org/wiki/Ganoderma_oregonense',
+  };
+
+  return (
+    <Page title="Home" description="greetings earthling" url="https://unforsaken.earth/search" isNotOpenSourceExample={true}>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          minHeight: '100vh',
+          padding: '24px',
+        }}
+      >
+        <div
+          style={{
+            maxWidth: '400px',
+            width: '100%',
+          }}
+        >
+          <div className={searchStyles.searchResultsContainer}>
+            <div
+              className={searchStyles.row}
+              style={{
+                gridTemplateColumns: '1fr',
+                maxWidth: '400px',
+                margin: '0 auto',
+              }}
+            >
+              <TaxaCard {...ganodermaOregonense} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Page>
+  );
 }
